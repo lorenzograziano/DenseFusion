073cd925e1b6be3c84509694e4adb5a52ce5bdeb
Lorenzo <lorenzograziano90@gmail.com>
2019-08-23 17:35:51 +0200

    unet architecture implemented

--- unet_segmentation/unet_evaluation.py
+++ unet_segmentation/unet_evaluation.py
@@ -6,7 +6,6 @@ import torch.optim as optim
 from PIL import Image, ImageDraw
 
 from unet_segmentation.dataset_unet import LinemodDataset
-from unet_segmentation.train import dir_checkpoint
 from unet_segmentation.unet import UNet
 
 
@@ -42,17 +41,18 @@ optimizer = optim.SGD(net.parameters(),
                       weight_decay=0.0005)
 
 criterion = nn.BCELoss()
-net.load_state_dict(torch.load(dir_checkpoint + "4CP10.pth"))
+net.load_state_dict(torch.load("/home/lorenzo/PycharmProjects/DenseFusionMasterAI/unet_segmentation/trained_models/4CP10.pth"))
 
 for i, data in enumerate(testloader, 0):
     if i < 5:
         inputs, labels = data
+        print(inputs.shape)
         inputs, labels = inputs.to(device), labels.to(device)
         outputs = net(inputs)
 
         outputs = outputs.cpu().detach().numpy()[0, 0, :, :] > 0.49
         labels = labels.cpu().detach().numpy()[0, 0, :, :]
-        inputs = (inputs.cpu().detach().numpy()[0, :, :, :]).reshape((240, 320, 3))
+        inputs = (inputs.cpu().detach().numpy()[0, :, :, :]).reshape((480, 640, 3))
 
         out = Image.fromarray(np.uint8(outputs * 255))
         lab = Image.fromarray(np.uint8(labels * 255))
